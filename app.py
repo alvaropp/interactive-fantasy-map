39ef1bbf56b27f01ff6d58c88e74885e619f54d8
Alvaro Perez-Diaz <alvpdiaz@gmail.com>
2020-05-23 13:49:17 +0100

    adding examples as iframes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flash, Flask, Markup, render_template, redirect, request
+from flask import flash, Flask, Markup, render_template, redirect, request, send_from_directory
 from form import MapForm
 from process_new_map import create_map_from_form
 
@@ -21,9 +21,24 @@ def home():
     return render_template("index.html", title="Create a new map", form=form)
 
 
+@app.route("/examples", methods=["GET", "POST"])
+def examples():
+    return render_template("examples.html")
+
+
+@app.route("/help", methods=["GET", "POST"])
+def help():
+    return "Yeah help..."
+
+
 @app.route("/maps/<map_uuid>/<map_name>.html")
 def show_map(map_uuid, map_name):
-    return render_template(f"map_template.html", data=[map_uuid, map_name])
+    return render_template("map_template.html", data=[map_uuid, map_name])
+
+
+@app.route("/examples/<example_name>.html")
+def show_example_map(example_name):
+    return render_template(f"example_{example_name}.html")
 
 
 if __name__ == "__main__":
